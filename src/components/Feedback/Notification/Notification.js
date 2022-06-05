41102d61ce2ee9a0467b3b5068875656ea637260
Anna Kulibabina <anna.kulibabina@gmail.com>
2022-06-05 16:31:42 +0300

    adds styles

---
+++ src/components/Feedback/Notification/Notification.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import s from './Notification.module.css';
+
+const Notification = ({ message }) => <h2 className={s.title}>{message}</h2>;
+
+export default Notification;
